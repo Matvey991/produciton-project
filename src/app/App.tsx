ab82ddba6b20a83efd97e413f29e160411c66c77
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-11-11 14:20:52 +0400

    improving the router architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import { useTheme } from "./Provider/ThemeProvider";
-import { Link, Route, Routes } from "react-router-dom";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import { Suspense } from "react";
+import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import "./styles/index.scss";
+import { AppRouter } from "./Provider/router";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,12 +12,7 @@ export const App = () => {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
