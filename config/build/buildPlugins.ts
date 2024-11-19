697d67a4d20b4d09a772ba78044c7809852dbd2d
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-11-19 22:10:18 +0400

    add react testing library, add tests for btn, sidebar

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,24 +1,28 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack, { DefinePlugin, HotModuleReplacementPlugin } from "webpack";
-import { BuildOptions } from "./types/config";
-import MiniCssExtraPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack, { DefinePlugin, HotModuleReplacementPlugin } from 'webpack'
+import { BuildOptions } from './types/config'
+import MiniCssExtraPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins({
-  paths,
-  isDev,
+    paths,
+    isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HtmlWebpackPlugin({
-      template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtraPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css",
-    }),
-    new DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new HotModuleReplacementPlugin(),
-  ];
+    return [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtraPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
+    ]
 }
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,25 +1,25 @@
-import { ButtonHTMLAttributes, FC } from "react";
-import cls from "./Button.module.scss";
-import { classNames } from "shared/lib/classNames/classNames";
+import { ButtonHTMLAttributes, FC } from 'react'
+import cls from './Button.module.scss'
+import { classNames } from 'shared/lib/classNames/classNames'
 
 export enum ThemeButton {
-  CLEAR = "clear",
+    CLEAR = 'clear',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string;
-  theme?: ThemeButton;
+    className?: string
+    theme?: ThemeButton
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-  const { className, theme, children, ...otherProps } = props;
+    const { className, theme, children, ...otherProps } = props
 
-  return (
-    <button
-      className={classNames(cls.Button, {}, [className, cls[theme]])}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
-};
+    return (
+        <button
+            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    )
+}
