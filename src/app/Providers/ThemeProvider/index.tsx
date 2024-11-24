1b212e62ded08112733d62238e6c4c0352dff445
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-11-24 23:01:21 +0400

    fix

--- src/app/Providers/ThemeProvider/index.tsx
+++
@@ -1,5 +0,0 @@
-import { ThemeProvider } from './ui/themeProvider'
-import { Theme } from './lib/ThemeContext'
-import { useTheme } from './lib/useTheme'
-
-export { ThemeProvider, Theme, useTheme }
