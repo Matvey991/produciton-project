f113cc692a603f99895e9563e86129abc4bca9f5
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-11-24 23:04:34 +0400

    fix

---
+++ src/app/Providers/ThemeProvider/index.tsx
@@ -0,0 +1,11 @@
+// import { ThemeProvider } from './ui/themeProvider'
+// import { Theme } from './lib/ThemeContext'
+// import { useTheme } from './lib/useTheme'
+
+// export { ThemeProvider, Theme, useTheme }
+
+import { Theme } from './lib/ThemeContext'
+import { ThemeProvider } from './ui/themeProvider'
+import { useTheme } from './lib/useTheme'
+
+export { ThemeProvider, Theme, useTheme }
