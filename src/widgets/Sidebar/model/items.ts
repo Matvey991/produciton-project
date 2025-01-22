1c4c0933bd059da5246bfec4d3824d1cf5cdf4b2
Matvey <matveyfedotov61@gmail.com>
2025-01-22 14:33:41 +0400

    fix sidebar

--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -4,35 +4,3 @@ import AboutIcon from 'shared/assets/icons/about.svg';
 import ProfileIcon from 'shared/assets/icons/Vector.svg';
 import ArticleIcon from 'shared/assets/icons/Article.svg';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
-
-export interface SidebarItemType {
-    path: string;
-    text: string;
-    Icon: React.VFC<React.SVGProps<SVGSVGElement>>;
-    authOnly?: boolean;
-}
-
-export const SidebarItemsList: SidebarItemType[] = [
-    {
-        path: RoutePath.main,
-        Icon: MainIcon,
-        text: 'Главная',
-    },
-    {
-        path: RoutePath.about,
-        Icon: AboutIcon,
-        text: 'О сайте',
-    },
-    {
-        path: RoutePath.profile,
-        Icon: ProfileIcon,
-        text: 'Профиль',
-        authOnly: true,
-    },
-    {
-        path: RoutePath.articles,
-        Icon: ArticleIcon,
-        text: 'Статьи',
-        authOnly: true,
-    },
-];
