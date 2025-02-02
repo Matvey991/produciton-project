623c388b821963a78262d04dc3a93fe148032eff
Matvey <matveyfedotov61@gmail.com>
2025-02-03 01:38:57 +0400

    production

--- src/shared/ui/Code/Code.tsx
+++ src/shared/ui/Code/Code.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable i18next/no-literal-string */
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
--- src/shared/ui/Dropdown/Dropdown.tsx
+++ src/shared/ui/Dropdown/Dropdown.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable i18next/no-literal-string */
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Fragment, memo, ReactNode } from 'react';
