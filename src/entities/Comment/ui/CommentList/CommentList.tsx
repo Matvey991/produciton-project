7d2034ebf7d4b2e17b1b8414d9ae095e145acfc4
Matvey <matveyfedotov61@gmail.com>
2025-02-12 23:08:21 +0400

    fix style problems

--- src/entities/Comment/ui/CommentList/CommentList.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -19,7 +19,7 @@ export const CommentList = memo((props: CommentListProps) => {
 
     if (isLoading) {
         return (
-            <VStack gap="16" className={classNames('', {}, [className])}>
+            <VStack gap="8" max className={classNames('', {}, [className])}>
                 <CommentCard isLoading />
                 <CommentCard isLoading />
                 <CommentCard isLoading />
@@ -28,7 +28,7 @@ export const CommentList = memo((props: CommentListProps) => {
     }
 
     return (
-        <VStack gap="16" max className={classNames('', {}, [className])}>
+        <VStack gap="8" max className={classNames('', {}, [className])}>
             {comments?.length
                 ? comments.map((comment) => (
                     <CommentCard
