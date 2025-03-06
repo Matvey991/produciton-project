ac0107086448cbc478e08cffe9eff6f8bee7c0c0
Matvey <matveyfedotov61@gmail.com>
2025-03-06 20:11:47 +0400

    skip one test, add stubs (fixtures)

--- cypress/support/commands/rating.ts
+++ cypress/support/commands/rating.ts
@@ -1,15 +1,13 @@
-import { User } from '../../../src/entities/User';
-
-export const setRate = (starsCount = 4, feedback = 'feedback') => {
+export const setRate = (starsCount = 5, feedback = 'feedback') => {
     cy.getByTestId(`StarRating.${starsCount}`).click();
     cy.getByTestId('RatingCard.Input').type(feedback);
     cy.getByTestId('RatingCard.Send').click();
 };
 
 declare global {
-    namespace Cypress {
+  namespace Cypress {
       interface Chainable {
-        addComment(starsCount: number, feedback: string): Chainable<User>
+          setRate(starsCount: number, feedback: string): Chainable<void>;
       }
-    }
   }
+}
